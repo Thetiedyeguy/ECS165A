7b2b4c1b51ed527d32b981e6042aaf183b8158f2
sofiacho <108560005+sofiacho@users.noreply.github.com>
2024-02-12 00:45:22 -0800

    Update query.py

--- lstore/query.py
+++ lstore/query.py
@@ -68,18 +68,21 @@ class Query:
     # Returns False if record locked by TPL
     # Assume that select will never be called on a key that doesn't exist
     """
+    
     def select(self, search_key, search_key_index, projected_columns_index):
+        
         output = []
         matchingRIDs = []
         returnColumns = []
-        if self.index.indices[column] != None: matchingRIDs = self.index.locate(search_key_index, search_key)
+    
+        matchingRIDs = table.get_rid(search_key_index, search_key)
+    
         if len(matchingRIDs) == 0: return []
-        for i in range(self.table.num_columns):
-            if projected_columns_index[i] == 0:
-                returnColumns[i] = None
-            newOut = Record(rid, search_key, returnColumns)
-            output.append(newOut)
-
+    
+        for rid in rids:
+            returnColumns.append(table.get_record(rid))
+    
+        return returnColumns #incomplete
     
     """
     # Read matching record with specified search key
