1c80f36ab1c2507b78a4f0142d8f59178b7a438a
Josiah Johnson <Thetiedyeguy42@gmail.com>
2024-02-11 17:21:51 -0800

    Update table.py

--- lstore/table.py
+++ lstore/table.py
@@ -27,9 +27,60 @@ class Table:
         self.num_columns = num_columns
         self.page_directory = {}
         self.index = Index(self)
+        self.records = 0
+        self.updates = 0
+        self.pool = {}
         pass
 
     def __merge(self):
         print("merge is happening")
         pass
- 
+
+    def get_page_location(self, type):
+        num_tail = self.updates
+        num_base = self.records - num_tail
+        page_range_idx = num_base % RECORD_PER_RANGE
+        if type == "base":
+            page_idx = num_base % RECORD_PER_PAGE
+        else:
+            page_idx = num_tail % RECORD_PER_PAGE
+        return page_range_idx, page_idx
+
+    def base_write(self, columns):
+        page_range_idx, page_idx = self.get_page_location('base')
+        for i, value in enumerate(columns):
+            id = (i, 'base', self.name, page_range_idx, page_idx)
+            page = get_page(id)
+
+            page.write(value)
+            offset = page.records - 1
+            self.pool[id] = page
+
+        rid = columns[0]
+        address = [offset, 'base', self.name, page_range_idx, page_idx]
+        self.page_directory[rid] = address
+        self.num_records += 1
+
+    def tail_write(self, columns):
+        page_range_idx, page_idx = self.get_page_location('tail')
+        for i, value in enumerate(columns):
+            id = (i, 'tail', self.name, page_range_idx, page_idx)
+            page = get_page(id)
+
+            page.write(value)
+            offset = page.records - 1
+            self.pool[id] = page
+
+        rid = columns[0]
+        address = [offset, 'base', self.name, page_range_idx, page_idx]
+        self.page_directory[rid] = address
+        self.records += 1
+        self.updates += 1
+
+    def get_page(self, id){
+        if id in self.pool:
+            return self.pool[id]
+        page = Page()
+        self.pool[id] = page
+        return page
+    }
