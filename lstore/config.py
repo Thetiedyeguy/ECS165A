7e30dd3ca971811d110fcf7623b9927c4796ac0c
sofiacho <108560005+sofiacho@users.noreply.github.com>
2024-02-13 22:18:38 -0800

    Update config.py

--- lstore/config.py
+++ lstore/config.py
@@ -6,4 +6,8 @@ RECORD_PER_RANGE = RECORD_PER_PAGE * PAGE_PER_RANGE
 METADATA = 4
 SPECIAL_NULL = (2 ** 64) - 1
 
-# ID = column, type, page number, page range number
+# record columns
+INDIRECTION_COLUMN = 0
+RID_COLUMN = 1
+TIMESTAMP_COLUMN = 2
+SCHEMA_ENCODING_COLUMN = 3
