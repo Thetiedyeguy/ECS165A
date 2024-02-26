d39e0fb078e045818e9456fedcfd5a71b7ba3869
Josiah Johnson <Thetiedyeguy42@gmail.com>
2024-02-25 18:33:40 -0800

    Update table.py

--- lstore/table.py
+++ lstore/table.py
@@ -2,6 +2,7 @@ from lstore.index import Index
 from time import time
 from lstore.page import Page, PageRange
 from lstore.config import *
+import time
 
 
 
@@ -29,36 +30,36 @@ class Table:
         self.updates = 0
         self.pool = {}
         self.key_to_rid = {}
+        self.rndbool = False
+        self.time = [0.0, 0.0]
         pass
 
     def __merge(self):
-        print("merge is happening")
         pass
 
-    def get_page_location(self, type):
-        num_tail = self.updates
+    def get_page_location(self, rid = -1):
         num_base = self.records
-        page_range_idx = num_base // RECORD_PER_RANGE
-        if(type == 'base'):
+        if(rid == -1):
+            page_range_idx = num_base // RECORD_PER_RANGE
             page_idx = num_base // RECORD_PER_PAGE
         else:
-            page_idx = num_tail // RECORD_PER_PAGE
+            page_range_idx = (rid - 906659671) // RECORD_PER_RANGE
+            page_idx =  -1
         page_idx = page_idx % PAGE_PER_RANGE
 
         return page_range_idx, page_idx
 
     def base_write(self, columns):
-        page_range_idx, page_idx = self.get_page_location('base')
+        page_range_idx, page_idx = self.get_page_location()
         for i, value in enumerate(columns):
-            id = (i, page_range_idx)
-            page = self.get_page(id, 'base')
+            page = self.get_page(page_range_idx, i, 'base')
 
-            pageRange = self.pool[id]
+            pageRange = self.pool[page_range_idx]
             page.write(value)
-            pageRange.base_pages[int(page_idx)] = page
-            offset = page.num_records - 1
-            self.pool[id] = pageRange
+            pageRange.base_pages[int(page_idx)][i] = page
+            self.pool[page_range_idx] = pageRange
 
+        offset = page.num_records - 1
         rid = columns[RID_COLUMN]
         address = [offset, 'base', page_range_idx, page_idx]
         self.page_directory[rid] = address
@@ -66,17 +67,16 @@ class Table:
         self.key_to_rid[columns[self.key + METADATA]] = rid
 
     def tail_write(self, columns):
-        page_range_idx, page_idx = self.get_page_location('tail')
+        page_range_idx, page_idx = self.get_page_location(columns[0])
         for i, value in enumerate(columns):
-            id = (i, page_range_idx)
-            page = self.get_page(id, 'tail')
+            pageRange = self.pool[page_range_idx]
+            page = self.get_page(page_range_idx, i, 'tail')
 
-            pageRange = self.pool[id]
             page.write(value)
-            pageRange.tail_pages[int(page_idx)] = page
-            offset = page.num_records - 1
-            self.pool[id] = pageRange
+            pageRange.tail_pages[pageRange.current_tail_idx - 1][i] = page
+            self.pool[page_range_idx] = pageRange
 
+        offset = page.num_records - 1
         rid = columns[RID_COLUMN]
         address = [offset, 'tail', page_range_idx, page_idx]
         self.page_directory[rid] = address
@@ -94,14 +94,12 @@ class Table:
         return self.key_to_rid[key]
 
     def get_value(self, column, address):
-        id = (column, address[2])
-        page = self.get_page(id, address[1], False, address[3])
+        page = self.get_page(address[2], column, address[1], False, address[3])
         value = page.get_value(address[0])
         return value
 
     def update_value(self, column, address, value):
-        id = (column, address[2])
-        page = self.get_page(id, address[1], False, address[3])
+        page = self.get_page(address[2], column, address[1], False, address[3])
         page.write(value, address[0])
 
     def get_record(self, rid):
@@ -112,20 +110,20 @@ class Table:
             record.append(result)
         return record
 
-    def get_page(self, id, type, current = True, pageNumber = 0):
-        if id in self.pool:
-            pageRange = self.pool[id]
+    def get_page(self, page_range_idx, column, type, current = True, pageNumber = 0):
+        if page_range_idx in self.pool:
+            pageRange = self.pool[page_range_idx]
         else:
-            pageRange = PageRange()
-            self.pool[id] = pageRange
+            pageRange = PageRange(self.num_columns + METADATA)
+            self.pool[page_range_idx] = pageRange
         if(current):
             if(type == 'base'):
-                page = pageRange.get_current_base()
+                page = pageRange.get_current_base(column)
             else:
-                page = pageRange.get_current_tail()
+                page = pageRange.get_current_tail(column)
         else:
             if(type == 'base'):
-                page = pageRange.base_pages[int(pageNumber)]
+                page = pageRange.base_pages[int(pageNumber)][column]
             else:
-                page = pageRange.tail_pages[int(pageNumber)]
+                page = pageRange.get_current_tail(column)
         return page
