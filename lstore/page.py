6719978eece31b165485b519370b95c798a3babd
Josiah Johnson <Thetiedyeguy42@gmail.com>
2024-02-27 23:23:14 -0800

    Update page.py

--- lstore/page.py
+++ lstore/page.py
@@ -6,6 +6,7 @@ class Page:
         self.num_records = 0
         self.data = bytearray(4096)
         self.tps = 0
+        self.dirty = True
 
     def has_capacity(self):
         return self.num_records < RECORD_PER_PAGE
@@ -16,10 +17,12 @@ class Page:
             if self.has_capacity():
                 self.data[location * 8:(location + 1) * 8] = int(value).to_bytes(8, byteorder = 'big')
                 self.num_records += 1
+                self.dirty = True
             else:
                 raise Exception("Page is full")
         else:
             self.data[location * 8:(location + 1) * 8] = int(value).to_bytes(8, byteorder = 'big')
+            self.dirty = True
         pass
 
     def get_value(self, location):
@@ -31,29 +34,26 @@ class PageRange:
         self.num_base_pages = num_base_pages
         self.base_pages = [[None for i in range(self.num_columns)] for _ in range(num_base_pages)] # 16 base pages with a physical page for each row
         self.tail_pages = []  # Start with a single tail page
-        self.dirty = [False for i in range (self.num_base_pages)]
+        self.base_dirty = [False for i in range (self.num_base_pages)]
+        self.tail_dirty = []
         self.current_base_idx = 0
         self.current_tail_idx = 0
+        self.dirty = True
         self.next = None
         self.prev = None
         self.idx = None
+        self.pinned = 0
 
-    def set_dirty(self, idx):
-        self.dirty[idx] = True
-
-    def set_all_dirty(self):
-        for i in range(self.num_base_pages):
-            self.set_dirty(i)
 
     def make_tail_page(self):
         self.tail_pages.append([Page() for _ in range(self.num_columns)])
-        self.set_dirty(self.current_tail_idx)
         self.current_tail_idx += 1
+        self.dirty = True
 
     def make_base_page(self, idx):
         self.base_pages[idx] = [Page() for _ in range(self.num_columns)]
-        self.set_dirty(self.current_base_idx)
         self.current_base_idx += 1
+        self.dirty = True
 
     def get_current_base(self, column):
         if(self.current_base_idx != 0):
